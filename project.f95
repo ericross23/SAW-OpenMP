251062fd85467ec6aa7197c9d74dacfc0db4ae02
ericross23 <eross1@alumni.nd.edu>
2016-05-02 09:55:59 -0400

    Working OpenMP version

--- project.f95
+++ project.f95
@@ -1,17 +1,27 @@
 program project
-
+        use omp_lib
         implicit none
         
         integer, dimension(:), allocatable :: saw_length
-        integer                            :: i, j, k, l, m, nmc, n_saw, counter
-        real, dimension(:,:), allocatable  :: pos
-        real                               :: rand, start, finish, avg_chains, distance, r_squared, mseted
-
+        integer                            :: seed
+        common /RAND/ SEED
+        integer, parameter                 :: NUM_THREADS = 4
+        integer                            :: max_threads
         
-        call init_random_seed()
+        !$omp threadprivate(/RAND/)
+        integer, parameter                 :: nmc = 1000
+        double precision                   :: randnum
+        integer                            :: i, j, k, l, m, n_saw, counter
+        real, dimension(:,:), allocatable  :: pos
+        real                               :: rand, start, finish, avg_chains, distance, r_squared, mseted                    
+        
 
         n_saw = 96
-        nmc = 1000
+        seed = 1
+        max_threads = omp_get_max_threads()
+
+        print*, max_threads
+
 
         allocate(saw_length(n_saw))
         allocate(pos(n_saw+3,3))
@@ -22,17 +32,18 @@ program project
         end do
 
         write(*,'(A11,A14,A20,A10)') "SAW length ", "Avg # SAW's ", "Mean square displacement ", "time"
-
+        !$omp parallel num_threads(NUM_THREADS)
+        seed = omp_get_thread_num() + 1
+        !$omp do private(pos), schedule(dynamic), reduction(+:avg_chains,mseted), collapse(1) 
         do i = 1, n_saw
                 avg_chains = 0
                 mseted = 0
-                call cpu_time(start)
+                start = omp_get_wtime()
                 do j = 1, nmc
-                        k = 2
                         pos(:,:) = 0
-                        counter = 1 
-                        do while (k <= saw_length(i))
-                                10 call random_number(rand)
+                        counter = 1
+                        10 do k = 2, saw_length(i) + 1
+                                rand = get_randnum()
                                 if (0 <= rand .and. rand < (1./6)) then
                                         pos(k,:) = pos(k-1,:) + (/ 1, 0, 0 /)        
                                 else if ((1./6) <= rand .and. rand < (2./6)) then
@@ -52,13 +63,11 @@ program project
                                         do m = l + 1, k
                                                 if (abs(pos(l,1) - pos(m,1)) < 1 .and. abs(pos(l,2) - pos(m,2)) < 1 &
                                                          .and. abs(pos(l,3) - pos(m,3)) < 1)  then
-                                                        k = 2
                                                         counter = counter + 1 
                                                         goto 10
                                                 end if
                                         end do
                                 end do
-                                k = k + 1
                         end do
                         avg_chains = avg_chains + counter
                         distance = sqrt(pos(saw_length(i),1) ** 2 + pos(saw_length(i),2) ** 2 + pos(saw_length(i),3) ** 2)
@@ -67,24 +76,21 @@ program project
                 end do
                 avg_chains = avg_chains / nmc
                 mseted = mseted / nmc
-                call cpu_time(finish)
+                finish = omp_get_wtime()
                 print*, saw_length(i),  avg_chains, mseted, finish - start        
         end do
+        !$end omp do
+        !$omp end parallel
+
+        contains
+
+        double precision function get_randnum() result(randnum)
+            integer :: seed
+            common /RAND/ SEED
+            
+            seed = seed * 65539
+            if (seed < 0) seed = (seed + 1) + 2147483647
+            randnum = seed * 0.4656613e-9
+        end function
  
-contains
-        subroutine init_random_seed()
-                implicit none
-                integer                          :: i, n, clock
-                integer,allocatable,dimension(:) :: seed
-
-                call random_seed(size = n)
-                allocate(seed(n))
-
-                call system_clock(count=clock)
-
-                seed = clock + 37 * (/ (i-1, i = 1, n) /)
-                call random_seed(put = seed)
-
-                deallocate(seed)
-        end subroutine
 end program
